9161e4b70e87ad74f43433f0ae50b6e5b905f030
Carlos Piña <carlosandrespina@gmail.com>
2018-10-23 17:46:25 -0500

    Implemented People and DocumentType tables

---
+++ database/migrations/2018_10_23_205230_create_people_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('lastname');
+            $table->unsignedInteger('typeDocument');
+            $table->string('document');
+            $table->string('phone');
+            $table->string('email')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
